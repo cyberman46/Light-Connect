cddbb158a2f5cfa3925fe1a9e569a36beff2f7e8
cyberman46 <akekapon27@gmail.com>
2018-01-02 12:55:14 +0700

    Initial Commit

---
+++ Light Connect/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Light Connect
+//
+//  Created by Akekapon Suebprom on 2/1/2561 BE.
+//  Copyright © 2561 kratostracking. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
