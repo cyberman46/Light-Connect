cc1ce32b6c26daec8e8477b6f55f5e59d091fc97
cyberman46 <akekapon27@gmail.com>
2018-01-03 16:52:14 +0700

    Code Demo

--- Light Connect/ViewController.swift
+++ Light Connect/ViewController.swift
@@ -7,12 +7,19 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var btnLight: UIButton!
+    var tapClick:Int = 0
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        if tapClick == 0 {
+            btnLight.setBackgroundImage(#imageLiteral(resourceName: "Light0"), for: .normal)
+        }else{
+            btnLight.setBackgroundImage(#imageLiteral(resourceName: "Light1"), for: .normal)
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +27,32 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func btnLightControl(_ sender: UIButton) {
+        
+        if tapClick == 0 {
+            tapClick = 1
+            btnLight.setBackgroundImage(#imageLiteral(resourceName: "Light1"), for: .normal)
+            
+            Alamofire.request("http://0.0.0.0/ledoff").response { response in
+                
+                if let data = response.data, let _ = String(data: data, encoding: .utf8) {
+                    print("Data: On")
+                }
+            }
+            
+        }else if tapClick == 1 {
+            tapClick = 0
+            btnLight.setBackgroundImage(#imageLiteral(resourceName: "Light0"), for: .normal)
+            Alamofire.request("http://0.0.0.0/ledon").response { response in
+                
+                if let data = response.data, let _ = String(data: data, encoding: .utf8) {
+                    print("Data: Off")
+                }
+            }
+        }
+        
+        print("Tap is : ",tapClick)
+    }
+    
 }
 
